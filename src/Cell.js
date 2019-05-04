8114bd2e3c1acc49cd1e5a90e362ac4197b95169
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-04 02:25:23 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
@@ -14,12 +14,12 @@ export default class Cell extends Component {
       color: this.props.getColor()
     })
   }
-  
+
   render() {
     return (
       <div className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
-  
+
 }
